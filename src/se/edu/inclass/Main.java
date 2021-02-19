4f784cff91a4da127f3ab741129f9a166d6e2877
LeeHanYongAndy <andyleehy2502@gmail.com>
2021-02-19 17:24:20 +0800

    update stages all

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -17,13 +17,17 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        System.out.println("Printing deadlines");
-        printDeadlines(tasksData);
+        //System.out.println("Printing deadlines");
+        //printDeadlines(tasksData);
+
+        //System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+
+        //printData(tasksData);
+        //printDataWithStreams(tasksData);
         printDeadlineUsingStreams(tasksData);
         System.out.println("Total number of deadlines (using streams): " +
                 countDeadlinesWithStreams(tasksData));
 
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
     }
 
@@ -37,10 +41,23 @@ public class Main {
         return count;
     }
 
+    private static int countDeadlinesWithStreams(ArrayList<Task> tasksData) {
+        int count = (int) tasksData.stream()
+                .filter((t) -> t instanceof Deadline) // predicate
+                .count();
+        return count;
+    }
+
     public static void printData(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             System.out.println(t);
         }
+    }
+
+    public static void printDataWithStreams(ArrayList<Task> tasksData) {
+        System.out.println("Printing tasks data with stream");
+        tasksData.stream()
+                .forEach(System.out::println);
     }
 
     public static void printDeadlines(ArrayList<Task> tasksData) {
@@ -49,5 +66,12 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlineUsingStreams(ArrayList<Task> tasks) {
+        System.out.println("Printing deadline with stream");
+        tasks.stream()
+                .filter((t) -> t instanceof Deadline) // predicate
+                .forEach(System.out::println);
     }
 }
